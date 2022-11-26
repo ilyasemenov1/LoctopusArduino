b9c26825dcac390890dfef21085db62ec097e7a9
Ilya Semenov <sialogacc@gmail.com>
2022-11-26 16:51:19 +0300

    Init commit

---
+++ main.ino
@@ -0,0 +1,22 @@
+
+const int controlMotorPort = 2;
+const int wifiInputPort = 1;
+
+
+int parseWifiMsg() {
+
+}
+
+int botRotateByDdeg(int deg) {
+    
+}
+
+void setup() {
+    pinMode(controlMotorPort, OUTPUT);
+    pinMode(wifiInputPort, INPUT);
+}
+
+void loop() {
+
+}
+
