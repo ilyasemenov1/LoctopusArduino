e982d2cebe0628c66e92ebdaad41b9e20fba6355
Ilya Semenov <sialogacc@gmail.com>
2022-11-26 18:21:29 +0300

    Add ultrasonic attemption

--- main.ino
+++ main.ino
@@ -1,8 +1,10 @@
 
-//#include <ESP8266WiFi.h>
+//#include <ESP8266WiFi.h> // include wifi lib
 #include <Servo.h>
+#include <HCSR04.h> // include ultrasonic lib
 
 Servo servo;
+HCSR04 hc(5, 6);
 
 const int controlMotorPort = 2;
 const int servoMotorPort = 3;
@@ -14,7 +16,18 @@ int parseWifiMsg() {
 }
 
 int botRotateByDder(char der) {
+    
+}
 
+int simpleUltrasonicAlgiritm() {
+    int dist = hc.dist();
+
+    if (dist > 30) {
+        digitalWrite(controlMotorPort, 190);
+    } else {
+        servo.write(90);
+        digitalWrite(controlMotorPort, 190);
+    }
     
 }
 
@@ -28,6 +41,6 @@ void setup() {
 }
 
 void loop() {
-
+    simpleUltrasonicAlgiritm();
 }
 
