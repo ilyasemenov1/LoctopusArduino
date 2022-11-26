159bdd3aa4e916acbb762b691e15927f8d07631b
Ilya Semenov <sialogacc@gmail.com>
2022-11-26 19:23:36 +0300

    Add wifiParse file

--- main.ino
+++ main.ino
@@ -2,6 +2,7 @@
 //#include <ESP8266WiFi.h> // include wifi lib
 #include <Servo.h>
 #include <HCSR04.h> // include ultrasonic lib
+//#include "wifiParse.ino"
 
 Servo servo;
 HCSR04 hc(5, 6);
@@ -10,10 +11,6 @@ const int controlMotorPort = 2;
 const int servoMotorPort = 3;
 const int wifiInputPort = 1;
 
-
-int parseWifiMsg() {
-
-}
 
 int botRotateByDder(char der) {
     
